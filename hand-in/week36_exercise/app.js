ee9610e35f2d6bb761fb57bcddafcfdb73505f77
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-09-05 14:11:28 +0200

    exercises for week 36

---
+++ hand-in/week36_exercise/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const weaponsList = [
+  { id: 1, name: "Shotgun", isAutomatic: false },
+  { id: 2, name: "Gun", isAutomatic: false },
+  { id: 3, name: "Automatic Rifle", isAutomatic: true },
+];
+
+//GET requests go here
+
+app.get("/weapons", (req, res) => {
+  res.send(weaponsList); //TODO returs as an array, not as a list.. Should maybe be done diffrently
+});
+
+app.get("/weapons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const weapon = weaponsList.find((weap) => weap.id === id);
+  res.send(weapon);
+});
+
+app.get("/new-weapon", (req, res) => {
+  const weapon = {
+    id: Number(req.query.id),
+    name: req.query.name,
+    isAutomatic: Boolean(req.query.isAutomatic),
+  };
+  weaponsList.push(weapon);
+  console.log(weaponsList);
+
+  res.send(`${weapon.name} has now been saved`);
+});
+
+
+//Post requests go here
+
+//Put requests go here
+
+//Delete requests go here
+
+
+
+
+app.listen(8080, () => {
+  console.log("Server is listning");
+});
